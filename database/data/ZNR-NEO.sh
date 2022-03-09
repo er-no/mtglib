c51bfdb5ccc67fd87a660cd65cf55dcb43dea6b5
Erik Nord <tpi13eno@student.lu.se>
2022-03-09 16:58:50 +0100

    data scraped and compiled to csv files

---
+++ database/data/ZNR-NEO.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# fetches standard format ZNR - NEO
+
+python3 ../data-scraper/scraper.py ZNR ZNR.csv
+python3 ../data-scraper/scraper.py KHM KHM.csv
+python3 ../data-scraper/scraper.py STX STX.csv
+python3 ../data-scraper/scraper.py AFR AFR.csv
+python3 ../data-scraper/scraper.py MID MID.csv
+python3 ../data-scraper/scraper.py VOW VOW.csv
+python3 ../data-scraper/scraper.py NEO NEO.csv
